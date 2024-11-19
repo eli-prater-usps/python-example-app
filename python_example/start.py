8418d86912d30c64ff2c05a936c10ea453a45ce6
Eli Prater <kkjnf0@Elis-MacBook-Pro.local>
2024-11-19 11:01:51 -0500

    apply fix

--- python_example/start.py
+++ python_example/start.py
@@ -8,10 +8,13 @@ def multiply_numbers(x, y):
     return x * y
 
 def divide_numbers(x, y):
-    return x / y -2
+    return x / y
 
 def main():
     print(add_numbers(5, 3))
     print(subtract_numbers(5, 3))
     print(multiply_numbers(5, 3))
     print(divide_numbers(5, 3))
+
+if __name__ == "__main__":
+    main()
